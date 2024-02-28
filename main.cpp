c402e7b683aeb2bf585a201b05695d6888e8b190
sillyfoolly <andrey.gromyko.97@gmail.com>
2024-02-28 12:30:57 +0300

    ////

--- main.cpp
+++ main.cpp
@@ -144,57 +144,8 @@
 //endregion
 
 //region TAKS5
-#include <GL/glut.h>
-
-void RenderScene(void) 
-{
-   glClear(GL_COLOR_BUFFER_BIT);
-   glColor3f(1.0f, 1.0f, 1.0f);
-   glRectf(-25.0f, 25.0f, 25.0f, -25.0f);
-   glFlush();
-}
-
-void SetupRC(void)
-{
-   glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
-}
-
-void ChangeSize(GLsizei w, GLsizei h)
-{
-   GLfloat aspectRatio;
-   if (h == 0)
-      h = 1;
-   glViewport(0, 0, w, h);
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   aspectRatio = (GLfloat)w / (GLfloat)h; if (w <= h)
-      glOrtho(-100.0, 100.0, -100 / aspectRatio, 100.0 / aspectRatio, 1.0, -1.0);
-   else
-       glOrtho(-100.0 * aspectRatio, 100.0 * aspectRatio, -100.0, 100.0, 1.0, -1.0);
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
-}
-
-int main(int args, char** argv)
-{
-   glutInit(&args, argv);
-   glutInitWindowPosition(100, 100);
-   glutInitWindowSize(400, 300);
-   // Установка режима отображения для окна, создаваемого библиотекой GLUT
-   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-   glutCreateWindow("Gromyko Andrey");
-   glutDisplayFunc(RenderScene);
-   glutReshapeFunc(ChangeSize);
-   SetupRC();
-   glutMainLoop();
-
-   return 0;
-}
-//endregion
-
-// region TASK6
 // #include <GL/glut.h>
-// 
+
 // void RenderScene(void) 
 // {
 //    glClear(GL_COLOR_BUFFER_BIT);
@@ -202,12 +153,62 @@ int main(int args, char** argv)
 //    glRectf(-25.0f, 25.0f, 25.0f, -25.0f);
 //    glFlush();
 // }
-// 
+
 // void SetupRC(void)
 // {
 //    glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
 // }
-// 
+
+// void ChangeSize(GLsizei w, GLsizei h)
+// {
+//    GLfloat aspectRatio;
+//    if (h == 0)
+//       h = 1;
+//    glViewport(0, 0, w, h);
+//    glMatrixMode(GL_PROJECTION);
+//    glLoadIdentity();
+//    aspectRatio = (GLfloat)w / (GLfloat)h; 
+//    if (w <= h)
+//       glOrtho(-100.0, 100.0, -100 / aspectRatio, 100.0 / aspectRatio, 1.0, -1.0);
+//    else
+//        glOrtho(-100.0 * aspectRatio, 100.0 * aspectRatio, -100.0, 100.0, 1.0, -1.0);
+//    glMatrixMode(GL_MODELVIEW);
+//    glLoadIdentity();
+// }
+
+// int main(int args, char** argv)
+// {
+//    glutInit(&args, argv);
+//    glutInitWindowPosition(100, 100);
+//    glutInitWindowSize(400, 300);
+//    // Установка режима отображения для окна, создаваемого библиотекой GLUT
+//    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+//    glutCreateWindow("Gromyko Andrey");
+//    glutDisplayFunc(RenderScene);
+//    glutReshapeFunc(ChangeSize);
+//    SetupRC();
+//    glutMainLoop();
+
+//    return 0;
+// }
+//endregion
+
+// region TASK6
+// #include <GL/glut.h>
+
+// void RenderScene(void) 
+// {
+//    glClear(GL_COLOR_BUFFER_BIT);
+//    glColor3f(1.0f, 1.0f, 1.0f);
+//    glRectf(-25.0f, 25.0f, 25.0f, -25.0f);
+//    glFlush();
+// }
+
+// void SetupRC(void)
+// {
+//    glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+// }
+
 // void ChangeSize(GLsizei w, GLsizei h)
 // {
 //    GLfloat aspectRatio;
@@ -220,7 +221,7 @@ int main(int args, char** argv)
 //    if (w < 200 ) glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // Красный цвет
 //    else if (w < 400) glClearColor(0.0f, 1.0f, 0.0f, 1.0f); // Зеленый цвет
 //    else glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // Синий цвет
-// 
+
 //    if (w <= h) {
 //        glOrtho(-100.0, 100.0, -100 / aspectRatio, 100.0 / aspectRatio, 1.0, -1.0);
 //    }
@@ -241,54 +242,59 @@ int main(int args, char** argv)
 //    glutReshapeFunc(ChangeSize);
 //    SetupRC();
 //    glutMainLoop();
-// 
+
 //    return 0;
 // }
 
 //region TASK7
-// #include <GL/glut.h>
-// 
-// void RenderScene(void) {
-//    glClear(GL_COLOR_BUFFER_BIT);
-//    glColor3f(0.27f, 1.0f, 0.95f);
-//    glRectf(-20.0f, 20.0f, -10.0f, 10.0f);
-//    glRectf(-10.0f, 20.0f, 10.f, 10.0f);
-//    glRectf(-20.0f, 10.0f, -10.0f, -10.0f);
-//    glRectf(-20.0f, -20.0f, 10.0f, -30.0f);
-//    glRectf(-10.0f, 0.0f, 10.0f, -10.0f);
-//    glRectf(0.0f, -10.0f, 10.0f, -20.0f);
-//    glColor3f(0.0f, 1.0f, 0.0f);
-//    glRectf(0.0f, 0.0f, 10.0f, -10.0f);
-//    glFlush();
-// }
-// 
-// void SetupRC(void) {
-//    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-// }
-// 
-// void ChangeSize(GLsizei w, GLsizei h) {
-//    GLfloat aspectRatio;
-//    if (h == 0)
-//        h = 1;
-//    glViewport(0, 0, w, h);
-//    glMatrixMode(GL_PROJECTION);
-//    glLoadIdentity();
-//    aspectRatio = (GLfloat)w / (GLfloat)h;
-//    if (w <= h)
-//        glOrtho(-40.0, 40.0, -40.0 / aspectRatio, 40.0 / aspectRatio, 1.0, -1.0);
-//    else
-//        glOrtho(-40.0 * aspectRatio, 40.0 * aspectRatio, -40.0, 40.0, 1.0, -1.0);
-//    glMatrixMode(GL_MODELVIEW);
-//    glLoadIdentity();
-// }
-// 
-// int main(int argc, char** argv) {
-//    glutInit(&argc, argv);
-//    glutCreateWindow("Gromyko Andrey");
-//    glutDisplayFunc(RenderScene);
-//    glutReshapeFunc(ChangeSize);
-//    SetupRC();
-//    glutMainLoop();
-//    return 0;
-// }
-////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+#include <GL/glut.h>
+
+void RenderScene(void) {
+   glClear(GL_COLOR_BUFFER_BIT);
+   glColor3f(0.27f, 1.0f, 0.95f);
+//    glRectf(-50.0, -50.0, 50, 50);
+   glRectf(-20.0f, 20.0f, -10.0f, 10.0f);
+   glRectf(-10.0f, 20.0f, 10.f, 10.0f);
+   glRectf(-20.0f, 10.0f, -10.0f, -10.0f);
+   glRectf(-20.0f, -20.0f, 10.0f, -30.0f);
+   glRectf(-10.0f, 0.0f, 10.0f, -10.0f);
+   glRectf(0.0f, -10.0f, 10.0f, -20.0f);
+   glColor3f(0.0f, 1.0f, 0.0f);
+   glRectf(0.0f, 0.0f, 10.0f, -10.0f);
+   glFlush();
+}
+
+void SetupRC(void) {
+   glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+}
+
+void ChangeSize(GLsizei w, GLsizei h) {
+   GLfloat aspectRatio;
+   if (h == 0)
+       h = 1;
+   glViewport(w/2, 0, w/2, h/2);
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   aspectRatio = (GLfloat)w / (GLfloat)h;
+   if (w <= h)
+       glOrtho(-40.0, 40.0, -40.0 / aspectRatio, 40.0 / aspectRatio, 1.0, -1.0);
+   else
+       glOrtho(-40.0 * aspectRatio, 40.0 * aspectRatio, -40.0, 40.0, 1.0, -1.0);
+    // glOrtho(-100,100,-100, 100,-1,1);
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+}
+
+int main(int argc, char** argv) {
+   glutInit(&argc, argv);
+   glutCreateWindow("Gromyko Andrey");
+   glutDisplayFunc(RenderScene);
+   glutReshapeFunc(ChangeSize);
+   glutInitWindowSize(400, 400);
+   SetupRC();
+   glutMainLoop();
+   return 0;
+}
+//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+//  200 300 400
