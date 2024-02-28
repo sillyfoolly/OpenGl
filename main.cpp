476158adbfbdd972f7ebc2215c478e332487b271
sillyfoolly <andrey.gromyko.97@gmail.com>
2024-02-28 10:30:14 +0300

    ,,,,

--- main.cpp
+++ main.cpp
@@ -192,57 +192,57 @@
 // }
 //endregion
 
-//region TASK6
-// #include <GL/glut.h>
-// 
-// void RenderScene(void) 
-// {
-//    glClear(GL_COLOR_BUFFER_BIT);
-//    glColor3f(1.0f, 1.0f, 1.0f);
-//    glRectf(-25.0f, 25.0f, 25.0f, -25.0f);
-//    glFlush();
-// }
-// 
-// void SetupRC(void)
-// {
-//    glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
-// }
-// 
-// void ChangeSize(GLsizei w, GLsizei h)
-// {
-//    GLfloat aspectRatio;
-//    if (h == 0) h = 1;
-//    glViewport(0, 0, w, h);
-//    glMatrixMode(GL_PROJECTION);
-//    glLoadIdentity();
-//    aspectRatio = (GLfloat)w / (GLfloat)h;
-//    if (w < 200 ) glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // Красный цвет
-//    else if (w < 400) glClearColor(0.0f, 1.0f, 0.0f, 1.0f); // Зеленый цвет
-//    else glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // Синий цвет
-// 
-//    if (w <= h) {
-//        glOrtho(-100.0, 100.0, -100 / aspectRatio, 100.0 / aspectRatio, 1.0, -1.0);
-//    }
-//    else {
-//        glOrtho(-100.0 * aspectRatio, 100.0 * aspectRatio, -100.0, 100.0, 1.0, -1.0);
-//        glMatrixMode(GL_MODELVIEW);
-//    }
-//    glLoadIdentity();
-// }
-// int main(int args, char** argv)
-// {
-//    glutInit(&args, argv);
-//    glutInitWindowPosition(100, 100);
-//    glutInitWindowSize(400, 300);
-//    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-//    glutCreateWindow("Gromyko Andrey");
-//    glutDisplayFunc(RenderScene);
-//    glutReshapeFunc(ChangeSize);
-//    SetupRC();
-//    glutMainLoop();
-// 
-//    return 0;
-// }
+// region TASK6
+#include <GL/glut.h>
+
+void RenderScene(void) 
+{
+   glClear(GL_COLOR_BUFFER_BIT);
+   glColor3f(1.0f, 1.0f, 1.0f);
+   glRectf(-25.0f, 25.0f, 25.0f, -25.0f);
+   glFlush();
+}
+
+void SetupRC(void)
+{
+   glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+}
+
+void ChangeSize(GLsizei w, GLsizei h)
+{
+   GLfloat aspectRatio;
+   if (h == 0) h = 1;
+   glViewport(0, 0, w, h);
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   aspectRatio = (GLfloat)w / (GLfloat)h;
+   if (w < 200 ) glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // Красный цвет
+   else if (w < 400) glClearColor(0.0f, 1.0f, 0.0f, 1.0f); // Зеленый цвет
+   else glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // Синий цвет
+
+   if (w <= h) {
+       glOrtho(-100.0, 100.0, -100 / aspectRatio, 100.0 / aspectRatio, 1.0, -1.0);
+   }
+   else {
+       glOrtho(-100.0 * aspectRatio, 100.0 * aspectRatio, -100.0, 100.0, 1.0, -1.0);
+       glMatrixMode(GL_MODELVIEW);
+   }
+   glLoadIdentity();
+}
+int main(int args, char** argv)
+{
+   glutInit(&args, argv);
+   glutInitWindowPosition(100, 100);
+   glutInitWindowSize(400, 300);
+   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+   glutCreateWindow("Gromyko Andrey");
+   glutDisplayFunc(RenderScene);
+   glutReshapeFunc(ChangeSize);
+   SetupRC();
+   glutMainLoop();
+
+   return 0;
+}
 
 //region TASK7
 // #include <GL/glut.h>
