b9a2e7044bd54c0009c0133cc0a6943f759118dc
sillyfoolly <andrey.gromyko.97@gmail.com>
2024-03-25 15:11:12 +0300

    ....

--- main.cpp
+++ main.cpp
@@ -550,195 +550,195 @@
 #pragma endregion LAB2_2
 
 #pragma region LAB3_1
-// #include <GL/glew.h>
-// #include <GL/glut.h>
-
-// double rotate_y = 0;
-// double rotate_x = 0;
-
-// void display() {
-//     // Очищаем буферы
-//     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-//     // Поворачиваем на угол
-//     glRotatef(rotate_x, 1.0, 0.0, 0.0);
-//     glRotatef(rotate_y, 0.0, 1.0, 0.0);
-//     //низ
-//     glBegin(GL_POLYGON);
-//     glColor3f(1.0, 1.0, 0.0);
-//     glVertex3f(0.5, -0.3, 0.5);
-//     glVertex3f(0.5, -0.3, -0.5);
-//     glVertex3f(-0.5, -0.3, -0.5);
-//     glVertex3f(-0.5, -0.3, 0.5);
-//     glEnd();
-//     //Задаем грань куба 2
-//     glBegin(GL_POLYGON);
-//     glColor3f(1.0, 1.0, 1.0);
-//     glVertex3f(0.5, -0.3, 0.5);
-//     glVertex3f(0.5, -0.3, -0.5);
-//     glVertex3f(0, 0.5, 0);
-//     glEnd();
-//     //Задаем грань куба 3
-//     glBegin(GL_POLYGON);
-//     glColor3f(1.0, 0.0, 1.0);
-//     glVertex3f(0.5, -0.3, -0.5);
-//     glVertex3f(-0.5, -0.3, -0.5);
-//     glVertex3f(0, 0.5, 0);
-//     glEnd();
-//     //Задаем грань куба 4
-//     glBegin(GL_POLYGON);
-//     glColor3f(0.0, 1.0, 0.0);
-//     glVertex3f(-0.5, -0.3, -0.5);
-//     glVertex3f(-0.5, -0.3, 0.5);
-//     glVertex3f(0, 0.5, 0);
-//     glEnd();
-//     //Задаем грань куба 5
-//     glBegin(GL_POLYGON);
-//     glColor3f(0.0, 0.0, 1.0);
-//     glVertex3f(-0.5, -0.3, 0.5);
-//     glVertex3f(0.5, -0.3, 0.5);
-//     glVertex3f(0, 0.5, 0);
-//     glEnd();
-//     glutSwapBuffers();
-// }
-
-// void specialKeys(int key, int x, int y) {
-//     // Меняем угол на 5 градусов
-//     if (key == GLUT_KEY_RIGHT){
-//         rotate_y = 3;
-//         rotate_x = 0;}
-//     else if (key == GLUT_KEY_LEFT){
-//         rotate_y = -3;
-//         rotate_x = 0;}
-//     else if (key == GLUT_KEY_UP){
-//         rotate_x = 6;
-//         rotate_y = 0;}
-//     else if (key == GLUT_KEY_DOWN){
-//         rotate_x = -6;
-//         rotate_y = 0;}
-//     glutPostRedisplay();
-// }
-
-// int main(int argc, char** argv) {
-//     glutInit(&argc, argv);
-//     glutCreateWindow("Gromyko Andrey");
-//     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-//     glEnable(GL_DEPTH_TEST);
-//     glutDisplayFunc(display);
-//     glutSpecialFunc(specialKeys);
-//     glutMainLoop();
-//     return 0;
-// }
-#pragma endregion
-
-#pragma region LAB3_2
 #include <GL/glew.h>
 #include <GL/glut.h>
-#include <math.h>
 
-// Вызывается для рисования сцены
-void RenderScene(void)
-{
-    // Угол поворота вокруг ядра
-    static GLfloat fElect1 = 0.0f;
-    // Очищаем окно текущим цветом очистки
+double rotate_y = 0;
+double rotate_x = 0;
+
+void display() {
+    // Очищаем буферы
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    //Обновляем матрицу наблюдения модели
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    //Транслируем всю сцену в поле зрения
-    //Это исходное преобразование наблюдения
-    glTranslatef(0.0f, 0.0f, -100.0f);
-    // Красное ядро
-    glColor3ub(255, 255, 255);
-    glutSolidSphere(10.0f, 150, 150);
-    // Желтые электроны
-    glColor3ub(255, 255, 0);
-    // Орбита первого электрона
-    // Записываем преобразование наблюдения
-    glPushMatrix();
-    glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
-    glTranslatef(90.0f, 0.0f, 0.0f);
-    // Рисуем электрон
-    glutSolidSphere(6.0f, 150, 150);
-    // Восстанавливаем преобразование наблюдения
-    glPopMatrix();
-    //Орбита второго электрона
-    glPushMatrix();
-    glRotatef(45.0f, 0.0f, 0.0f, 1.0f);
-    glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
-    glTranslatef(-70.0f, 0.0f, 0.0f);
-    glutSolidSphere(6.0f, 15, 15);
-    glPopMatrix();
-    // Орбита третьего электрона
-    glPushMatrix();
-    glRotatef(360.0f - 45.0f, 0.0f, 0.0f, 1.0f);
-    glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
-    glTranslatef(0.0f, 0.0f, 60.0f);
-    glutSolidSphere(6.0f, 15, 15);
-    glPopMatrix();
-    // Увеличиваем угол поворота
-    fElect1 += 10.0f;
-    if (fElect1 > 360.0f)
-    fElect1 = 0.0f;
-    // Показываем построенное изображение
+
+    // Поворачиваем на угол
+    glRotatef(rotate_x, 1.0, 0.0, 0.0);
+    glRotatef(rotate_y, 0.0, 1.0, 0.0);
+    //низ
+    glBegin(GL_POLYGON);
+    glColor3f(1.0, 1.0, 0.0);
+    glVertex3f(0.5, -0.3, 0.5);
+    glVertex3f(0.5, -0.3, -0.5);
+    glVertex3f(-0.5, -0.3, -0.5);
+    glVertex3f(-0.5, -0.3, 0.5);
+    glEnd();
+    //Задаем грань куба 2
+    glBegin(GL_POLYGON);
+    glColor3f(1.0, 1.0, 1.0);
+    glVertex3f(0.5, -0.3, 0.5);
+    glVertex3f(0.5, -0.3, -0.5);
+    glVertex3f(0, 0.5, 0);
+    glEnd();
+    //Задаем грань куба 3
+    glBegin(GL_POLYGON);
+    glColor3f(1.0, 0.0, 1.0);
+    glVertex3f(0.5, -0.3, -0.5);
+    glVertex3f(-0.5, -0.3, -0.5);
+    glVertex3f(0, 0.5, 0);
+    glEnd();
+    //Задаем грань куба 4
+    glBegin(GL_POLYGON);
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(-0.5, -0.3, -0.5);
+    glVertex3f(-0.5, -0.3, 0.5);
+    glVertex3f(0, 0.5, 0);
+    glEnd();
+    //Задаем грань куба 5
+    glBegin(GL_POLYGON);
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f(-0.5, -0.3, 0.5);
+    glVertex3f(0.5, -0.3, 0.5);
+    glVertex3f(0, 0.5, 0);
+    glEnd();
     glutSwapBuffers();
 }
 
-// Функция выполняет необходимую инициализацию
-// в контексте визуализации
-void SetupRC()
-{
-    glEnable(GL_DEPTH_TEST); // Удаление скрытых поверхностей
-    glFrontFace(GL_CCW); // Полигоны с обходом против
-    //часовой стрелки направлены наружу
-    glEnable(GL_CULL_FACE); //Внутри пирамиды расчеты не //производятся
-    // Черный фон
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-}
-
-void TimerFunc(int value)
-{
+void specialKeys(int key, int x, int y) {
+    // Меняем угол на 5 градусов
+    if (key == GLUT_KEY_RIGHT){
+        rotate_y = 3;
+        rotate_x = 0;}
+    else if (key == GLUT_KEY_LEFT){
+        rotate_y = -3;
+        rotate_x = 0;}
+    else if (key == GLUT_KEY_UP){
+        rotate_x = 6;
+        rotate_y = 0;}
+    else if (key == GLUT_KEY_DOWN){
+        rotate_x = -6;
+        rotate_y = 0;}
     glutPostRedisplay();
-    glutTimerFunc(100, TimerFunc, 1);
 }
 
-void ChangeSize(int w, int h)
-{
-    GLfloat nRange = 100.0f;
-    // Предотвращение деления на ноль
-    if (h == 0)
-    h = 1;
-    // Устанавливает поле просмотра по размерам окна
-    glViewport(0, 0, w, h);
-    // Обновляет стек матрицы проектирования
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    // Устанавливает объем отсечения с помощью отсекающих
-    // плоскостей (левая, правая, нижняя, верхняя,
-    // ближняя, дальняя)
-    if (w <= h)
-    glOrtho(-nRange, nRange, nRange * h / w, -nRange * h / w, -nRange * 2.0f, nRange *
-    2.0f);
-    else
-    glOrtho(-nRange * w / h, nRange * w / h, nRange, -nRange, -nRange * 2.0f, nRange *
-    2.0f);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-}
-
-int main(int argc, char* argv[])
-{
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
+    glutCreateWindow("Gromyko Andrey");
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(800, 600);
-    glutCreateWindow("OpenGL Atom");
-    glutReshapeFunc(ChangeSize);
-    glutDisplayFunc(RenderScene);
-    glutTimerFunc(500, TimerFunc, 1);
-    SetupRC();
+    glEnable(GL_DEPTH_TEST);
+    glutDisplayFunc(display);
+    glutSpecialFunc(specialKeys);
     glutMainLoop();
     return 0;
 }
+#pragma endregion
+
+#pragma region LAB3_2
+// #include <GL/glew.h>
+// #include <GL/glut.h>
+// #include <math.h>
+
+// // Вызывается для рисования сцены
+// void RenderScene(void)
+// {
+//     // Угол поворота вокруг ядра
+//     static GLfloat fElect1 = 0.0f;
+//     // Очищаем окно текущим цветом очистки
+//     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//     //Обновляем матрицу наблюдения модели
+//     glMatrixMode(GL_MODELVIEW);
+//     glLoadIdentity();
+//     //Транслируем всю сцену в поле зрения
+//     //Это исходное преобразование наблюдения
+//     glTranslatef(0.0f, 0.0f, -100.0f);
+//     // Красное ядро
+//     glColor3ub(255, 255, 255);
+//     glutSolidSphere(10.0f, 150, 150);
+//     // Желтые электроны
+//     glColor3ub(255, 255, 0);
+//     // Орбита первого электрона
+//     // Записываем преобразование наблюдения
+//     glPushMatrix();
+//     glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
+//     glTranslatef(90.0f, 0.0f, 0.0f);
+//     // Рисуем электрон
+//     glutSolidSphere(6.0f, 150, 150);
+//     // Восстанавливаем преобразование наблюдения
+//     glPopMatrix();
+//     //Орбита второго электрона
+//     glPushMatrix();
+//     glRotatef(45.0f, 0.0f, 0.0f, 1.0f);
+//     glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
+//     glTranslatef(-70.0f, 0.0f, 0.0f);
+//     glutSolidSphere(6.0f, 15, 15);
+//     glPopMatrix();
+//     // Орбита третьего электрона
+//     glPushMatrix();
+//     glRotatef(360.0f - 45.0f, 0.0f, 0.0f, 1.0f);
+//     glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
+//     glTranslatef(0.0f, 0.0f, 60.0f);
+//     glutSolidSphere(6.0f, 15, 15);
+//     glPopMatrix();
+//     // Увеличиваем угол поворота
+//     fElect1 += 10.0f;
+//     if (fElect1 > 360.0f)
+//     fElect1 = 0.0f;
+//     // Показываем построенное изображение
+//     glutSwapBuffers();
+// }
+
+// // Функция выполняет необходимую инициализацию
+// // в контексте визуализации
+// void SetupRC()
+// {
+//     glEnable(GL_DEPTH_TEST); // Удаление скрытых поверхностей
+//     glFrontFace(GL_CCW); // Полигоны с обходом против
+//     //часовой стрелки направлены наружу
+//     glEnable(GL_CULL_FACE); //Внутри пирамиды расчеты не //производятся
+//     // Черный фон
+//     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+// }
+
+// void TimerFunc(int value)
+// {
+//     glutPostRedisplay();
+//     glutTimerFunc(100, TimerFunc, 1);
+// }
+
+// void ChangeSize(int w, int h)
+// {
+//     GLfloat nRange = 100.0f;
+//     // Предотвращение деления на ноль
+//     if (h == 0)
+//     h = 1;
+//     // Устанавливает поле просмотра по размерам окна
+//     glViewport(0, 0, w, h);
+//     // Обновляет стек матрицы проектирования
+//     glMatrixMode(GL_PROJECTION);
+//     glLoadIdentity();
+//     // Устанавливает объем отсечения с помощью отсекающих
+//     // плоскостей (левая, правая, нижняя, верхняя,
+//     // ближняя, дальняя)
+//     if (w <= h)
+//     glOrtho(-nRange, nRange, nRange * h / w, -nRange * h / w, -nRange * 2.0f, nRange *
+//     2.0f);
+//     else
+//     glOrtho(-nRange * w / h, nRange * w / h, nRange, -nRange, -nRange * 2.0f, nRange *
+//     2.0f);
+//     glMatrixMode(GL_MODELVIEW);
+//     glLoadIdentity();
+// }
+
+// int main(int argc, char* argv[])
+// {
+//     glutInit(&argc, argv);
+//     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+//     glutInitWindowSize(800, 600);
+//     glutCreateWindow("OpenGL Atom");
+//     glutReshapeFunc(ChangeSize);
+//     glutDisplayFunc(RenderScene);
+//     glutTimerFunc(500, TimerFunc, 1);
+//     SetupRC();
+//     glutMainLoop();
+//     return 0;
+// }
 
 #pragma endregion
